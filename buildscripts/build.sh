4110256ccebfcab27def675e0bf618d7bfd0b180
Wim Devos <wim@obiwan.be>
2017-01-21 19:29:29 +0100

    Added Mono.Posix.dll

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -8,8 +8,10 @@ rm -rf *
 
 mkdir -p "./FxSchoolCalculus/"
 cp -rf "$DIR/../FxSchoolCalculus/bin/Release/." "./FxSchoolCalculus/"
+cp -rf "$DIR"/../win/* "./FxSchoolCalculus/"
 
 mkdir -p "./FxSchoolDictate/"
 cp -rf "$DIR/../FxSchoolDictate/bin/Release/." "./FxSchoolDictate/"
+cp -rf "$DIR"/../win/* "./FxSchoolDictate/"
 
 zip -r "./FxSchool.zip" *
